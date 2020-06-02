2fc6c8a1c3c5cc7f36667f2ab505e701408c233a
heartwoodnon <heartwoodnon@qq.com>
2020-06-02 18:32:25 +0800

    Account exit

--- DSA_Study/src/main/java/com/first/Test.java
+++ DSA_Study/src/main/java/com/first/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("commit test");
         System.out.println("share test");
+        System.out.println("3");
     }
 }
