6e8b8b19057293dc9cde67074f17c62f57d0837f
heartwoodnon <heartwoodnon@qq.com>
2020-06-26 22:08:51 +0800

    2020 06 26

--- DSA_Study/src/main/java/com/first/Test.java
+++ DSA_Study/src/main/java/com/first/Test.java
@@ -6,8 +6,6 @@ package com.first;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("commit test");
-        System.out.println("share test");
-        System.out.println("3");
+        System.out.println("commit and push test");
     }
 }
